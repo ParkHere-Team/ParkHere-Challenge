fd4ffe05569529686663a93716c60e296799a4a4
shakif95 <shakif.malek@gmail.com>
2025-03-11 13:41:33 +0100

    chore: init

---
+++ src/test/kotlin/eu/parkHere/challenge/ChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.parkHere.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
